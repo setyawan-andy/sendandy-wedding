e38a157a623a88733444210a4f114dea0544428e
setyawan-andy <andy.asbi@gmail.com>
2022-10-29 17:36:59 +0700

    initial set up wedding

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <iframe
+        allowFullScreen="allowfullscreen"
+        scrolling="no"
+        className="fp-iframe"
+        src="https://heyzine.com/flip-book/37061099ed.html"
+        style={{
+          border: "1px solid lightgray",
+          width: "100%",
+          height: "100vh"
+        }}
+      ></iframe>
     </div>
   );
 }
